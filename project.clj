3c90e56a4bece709a19d769a922b95b0067462ae
Joe Hayes <joephayes@gmail.com>
2017-09-14 19:01:26 -0400

    adds MIT license

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject lein-ftp-static-deploy "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :eval-in-leiningen true)
+  :description "A Leiningen plugin to deploy a directory as a static website via FTP."
+  :url "http://github.com/joephayes/lein-ftp-static-deploy"
+  :license {:name "The MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+  :eval-in-leiningen true
+  :scm  {:name  "git"
+         :url  "https://github.com/joephayes/lein-ftp-static-deploy"})
