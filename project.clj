c050564c8a45d4478fcb11c91c3fee94b26a9a26
Joe Hayes <joephayes@gmail.com>
2017-09-16 01:37:10 -0400

    Version 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-ftp-static-deploy "0.1.0"
+(defproject lein-ftp-static-deploy "0.1.1-SNAPSHOT"
   :description "A Leiningen plugin to deploy a directory as a static website via FTP."
   :url "http://github.com/joephayes/lein-ftp-static-deploy"
   :license {:name "The MIT License"
